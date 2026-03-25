66907406e4a9699b590cb0c20d40fc0dfdeec356
egantech05 <eganxhart@gmail.com>
2026-03-26 12:33:36 +1300

    adding meta color in index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ import { registerRootComponent } from 'expo';
 
 import App from './App';
 
-
+<meta name="theme-color" content="#1b1b1b" />
 // registerRootComponent calls AppRegistry.registerComponent('main', () => App);
 // It also ensures that whether you load the app in Expo Go or in a native build,
 // the environment is set up appropriately
