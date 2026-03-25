d3d33fb89e9284698bd7f1778c64585957d5a59b
egantech05 <eganxhart@gmail.com>
2026-03-26 12:43:56 +1300

    styling in pwa

--- app/+html.tsx
+++ app/+html.tsx
@@ -9,38 +9,27 @@ import { ScrollViewStyleReset } from 'expo-router/html';
 export default function Root({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
-      <head>
-        <meta charSet="utf-8" />
-        <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
-        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
-        <meta charSet="utf-8" />
-        <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
-        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+    <head>
+  <meta charSet="utf-8" />
+  <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
 
-        <meta name="theme-color" content="#1b1b1b" />
-        <meta name="apple-mobile-web-app-capable" content="yes" />
-        <meta name="apple-mobile-web-app-status-bar-style" content="black-translucent" />
-        <meta name="mobile-web-app-capable" content="yes" />
+  <meta name="theme-color" content="#1b1b1b" />
+  <meta name="theme-color" media="(prefers-color-scheme: dark)" content="#1b1b1b" />
+  <meta name="theme-color" media="(prefers-color-scheme: light)" content="#1b1b1b" />
 
-        <meta name="theme-color" content="#1b1b1b" />
-        <meta name="theme-color" media="(prefers-color-scheme: dark)" content="#1b1b1b" />
-        <meta name="theme-color" media="(prefers-color-scheme: light)" content="#1b1b1b" />
+  <meta name="apple-mobile-web-app-capable" content="yes" />
+  <meta name="apple-mobile-web-app-status-bar-style" content="black-translucent" />
+  <meta name="mobile-web-app-capable" content="yes" />
 
+  <ScrollViewStyleReset />
 
-        {/* 
-          Disable body scrolling on web. This makes ScrollView components work closer to how they do on native. 
-          However, body scrolling is often nice to have for mobile web. If you want to enable it, remove this line.
-        */}
-        <ScrollViewStyleReset />
-
-        {/* Add any additional <head> elements that you want globally available on web... */}
-
-        <style>{`
-          html, body {
-            background: #1b1b1b;
-          }
-        `}</style>
-      </head>
+  <style>{`
+    html, body {
+      background: #1b1b1b;
+    }
+  `}</style>
+</head>
       <body>{children}</body>
     </html>
   );
